b66ce2ed01f7f07e744307008551a69a2810d64b
elizabeth.goullaud <egoullaud@gmail.com>
2022-08-09 12:06:19 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Write your JavaScript below this line
+var name = "Elizabeth Goullaud";
+var favoriteSnack = "iced coffee";
+console.log(
+  "Hi, my name is " +
+    name +
+    ". I wish I had an " +
+    favoriteSnack +
+    " right now."
+);
+
+var numberOfHorses = 5;
+console.log("I wish I had " + numberOfHorses + " horses.");
+
+var favoriteFlower = "Dahlia";
+console.log("My favorite flower is a " + favoriteFlower + ".");
+
+var cityName = "Minneapolis";
+var birthDate = "February 14";
+console.log("I was born in " + cityName + " on " + birthDate + ".");
+
+var applesBananas = 5 + 8;
+console.log(applesBananas);
+
+var monthlyIncome = 4000;
+var monthlyRent = Number(prompt("how much is rent each month?"));
+
+console.log(
+  "Each month, my income is $${monthlyIncome} and my rent is $${monthlyRent}."
+);
